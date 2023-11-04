3915a091a4fd59aec4ffdb5c557bad5bc31f9397
LeThaiNhuQuynh <lethainhuquynh@gmail.com>
2023-11-04 12:46:38 +0700

    Log in screen - UI

---
+++ lib/presentation/log-in/LogIn.dart
@@ -0,0 +1,296 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class LogIn extends StatefulWidget {
+  const LogIn({super.key});
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: PreferredSize(
+              preferredSize: Size.fromHeight(65.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: const Border(
+                    bottom: BorderSide(
+                      color: Colors.grey,
+                      width: 0.3,
+                    ),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.4),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: AppBar(
+                  backgroundColor: Colors.white,
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 10),
+                        child: SvgPicture.asset(
+                          'assets/svgs/logo.svg',
+                          width: 35,
+                          height: 35,
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.7),
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(50.0),
+                          child: SvgPicture.asset(
+                            'assets/svgs/nation.svg',
+                            width: 25,
+                            height: 25,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            body: SingleChildScrollView(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      margin:
+                          const EdgeInsets.only(right: 20, top: 20, bottom: 30),
+                      child: Image.asset(
+                        'assets/images/log-in.png',
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 20.0),
+                      child: const Text(
+                        'Say hello to your English tutors',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 33,
+                          height: 1.2,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w800,
+                          color: Color.fromRGBO(0, 113, 240, 1),
+                        ),
+                      ),
+                    ),
+                    // Some Text
+                    Container(
+                      margin: const EdgeInsets.only(top: 5, bottom: 5),
+                      child: const Text(
+                        'Become fluent faster through one on one video chat lessons tailored to your goals.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 16,
+                          height: 1.3,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w600,
+                          color: Color.fromRGBO(0, 0, 0, .9),
+                        ),
+                      ),
+                    ),
+                    // Login Fields
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          Column(
+                            children: <Widget>[
+                              const Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  'EMAIL',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color.fromRGBO(0, 0, 0, .6),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  hintText: 'mail@example.com',
+                                  contentPadding: const EdgeInsets.symmetric(
+                                      vertical: 15.0, horizontal: 10.0),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5.0),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 25),
+                          Column(
+                            children: <Widget>[
+                              const Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  'PASSWORD',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color.fromRGBO(0, 0, 0, .6),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  contentPadding: const EdgeInsets.symmetric(
+                                      vertical: 15.0, horizontal: 10.0),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5.0),
+                                  ),
+                                  suffixIcon: IconButton(
+                                    onPressed: () {
+                                      // do nothing
+                                    },
+                                    icon: SizedBox(
+                                      width: 16,
+                                      height: 16,
+                                      child: SvgPicture.asset(
+                                        'assets/svgs/password.svg',
+                                        semanticsLabel: 'Password Icon',
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 25),
+                          Column(
+                            children: <Widget>[
+                              const Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  'Forgot Password?',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                    color: Color.fromRGBO(40, 106, 210, 1),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              SizedBox(
+                                height: 50,
+                                width: double.infinity,
+                                child: TextButton(
+                                  style: TextButton.styleFrom(
+                                    backgroundColor:
+                                        const Color.fromRGBO(0, 113, 240, 1),
+                                  ),
+                                  onPressed: () {
+                                    // do nothing
+                                  },
+                                  child: const Text(
+                                    'LOG IN',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w800,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 30),
+                          Column(
+                            children: <Widget>[
+                              const Text(
+                                'Or continue with',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              const SizedBox(height: 20),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SvgPicture.asset(
+                                    'assets/svgs/fb.svg',
+                                    width: 40,
+                                    height: 40,
+                                  ),
+                                  const SizedBox(width: 25),
+                                  SvgPicture.asset(
+                                    'assets/svgs/google.svg',
+                                    width: 40,
+                                    height: 40,
+                                  ),
+                                  const SizedBox(width: 25),
+                                  ClipOval(
+                                    child: Container(
+                                      padding: const EdgeInsets.all(5.0),
+                                      width: 40,
+                                      height: 40,
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        border: Border.all(
+                                          color: const Color.fromRGBO(
+                                              40, 106, 210, 1),
+                                          width: 1.0,
+                                        ),
+                                      ),
+                                      child: SvgPicture.asset(
+                                          'assets/svgs/mobile-phone-2642.svg'),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(height: 20),
+                              const Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Not a member yet?',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  SizedBox(width: 10),
+                                  Text(
+                                    'Sign up',
+                                    style: TextStyle(
+                                      color: Color(0xFF1890FF),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 30),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )));
+  }
+}
